31f0496f86e57d69cdaceb721c34b906086d54f9
Nathan Thomas <nwthomas@me.com>
2019-01-11 10:44:27 -0800

    Complete dog site with randomly generated pictures from API

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,89 +1,43 @@
 import React, { Component } from "react";
-import Character from "./components/CharacterComponent/Character";
+import Dog from "./components/DogComponent/Dog";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: [],
-      currentPage: 1
+      randomDogs: []
     };
   }
 
   componentDidMount() {
-    this.getCharacters();
+    this.getCharacters("https://dog.ceo/api/breeds/image/random/10");
   }
 
-  getCharacters = () => {
+  getCharacters = URL => {
     // feel free to research what this code is doing.
     // At a high level we are calling an API to fetch some starwars data from the open web.
     // We then take that data and resolve it our state.
-    const URL = `https://swapi.co/api/people/?page=${this.state.currentPage}`;
     fetch(URL)
       .then(res => {
         return res.json();
       })
       .then(data => {
-        this.setState({ starwarsChars: data.results });
+        this.setState({ randomDogs: data.message });
       })
       .catch(err => {
         throw new Error(err);
       });
   };
 
-  nextPage = event => {
-    document.body.scrollTop = document.documentElement.scrollTop = 0;
-    if (event.target.value === "previous") {
-      if (this.state.currentPage > 1) {
-        this.setState(
-          {
-            currentPage: this.state.currentPage - 1
-          },
-          () => this.getCharacters()
-        );
-      } else {
-        return;
-      }
-    } else {
-      this.setState(
-        {
-          currentPage: this.state.currentPage + 1
-        },
-        () => this.getCharacters()
-      );
-    }
-  };
-
   render() {
-    console.log(this.state.starwarsChars);
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
-        <div className="character-list__container">
-          {this.state.starwarsChars.map((character, index) => {
-            return (
-              <Character key={"character" + index} character={character} />
-            );
+        <h1 className="Header">Doggos</h1>
+        <div className="dog-list__container">
+          {this.state.randomDogs.map((dogImg, index) => {
+            return <Dog dogImg={dogImg} key={"dog" + index} />;
           })}
-        </div>
-        <div className="pagination__buttons">
-          <button
-            onClick={this.nextPage}
-            type="button"
-            className="back__button button"
-            value="previous"
-          >
-            Previous
-          </button>
-          <button
-            onClick={this.nextPage}
-            type="button"
-            className="next__button button"
-            value="next"
-          >
-            Next
-          </button>
         </div>
       </div>
     );
